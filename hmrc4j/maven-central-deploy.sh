0e28a244d1cde52fd45e5e8088e9c0fdb9aa3768
Shane Gibson <shane.gibson@digital.hmrc.gov.uk>
2016-01-06 08:22:36 +0545

    preparing for mavencentral release

--- hmrc4j/maven-central-deploy.sh
+++ hmrc4j/maven-central-deploy.sh
@@ -1,9 +1,9 @@
-!/bin/bash
+#!/bin/bash
 # Deploy maven artefact in current directory into Maven central repository
 # using maven-release-plugin goals
 read -p "Really deploy to maven central repository  (yes/no)? "
 if ( [ "$REPLY" == "yes" ] ) then
-  ssh-add ~/.ssh/lubos.krnac
+  ssh-add ~/.ssh/shane.krnac
   ssh-add -l
   mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
   ssh-add -D
